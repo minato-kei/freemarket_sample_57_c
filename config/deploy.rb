83df8d73f0a3a4b218792d05f648183bbcea34b0
minato-kei <sweetness.deathcream@gmail.com>
2019-08-25 21:19:52 +0900

    unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :application, 'freemarket_sample_57_c'
 set :repo_url,  'git@github.com:minato-kei/freemarket_sample_57_c.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'shared/tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
