85445a1173451220f7dd1d5929fa5cb8b9550899
minato-kei <sweetness.deathcream@gmail.com>
2019-08-26 12:17:27 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,19 @@
-# if Rails.env.production?
-#   require 'carrierwave/storage/abstract'
-#   require 'carrierwave/storage/file'
-#   require 'carrierwave/storage/fog'
+if Rails.env.production?
+  require 'carrierwave/storage/abstract'
+  require 'carrierwave/storage/file'
+  require 'carrierwave/storage/fog'
 
-#   CarrierWave.configure do |config|
-#     config.storage = :fog
-#     config.fog_provider = 'fog/aws'
-#     config.fog_credentials = {
-#       provider: 'AWS',
-#       aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-#       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-#       region: 'ap-northeast-1' 
-#     }
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region: 'ap-northeast-1' 
+    }
 
-#     config.fog_directory  = ''
-#     config.asset_host = 'https://s3-ここにリージョン名を入れます(※例 ap-northeast-1).amazonaws.com/ここにバケット名を入れます'
-#   end
-# end
+    config.fog_directory  = 'mercari-fake-57c'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercari-fake-57c'
+  end
+end
