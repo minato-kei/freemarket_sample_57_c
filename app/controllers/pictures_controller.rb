a87222d10a9932d248a9f39c7e1834e6302bcb3a
minato-kei <sweetness.deathcream@gmail.com>
2019-08-26 13:27:53 +0900

    Update pictures_controller.rb

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,7 +6,7 @@ class PicturesController < ApplicationController
   end
   
   def create
-    # binding.pry
+    binding.pry
     @picture = Picture.new(picture_params)
     if @picture.save
       redirect_to root_path
