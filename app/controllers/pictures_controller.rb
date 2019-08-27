fc4eb5a66202983d9d1c897f6af28b3ce88aa1a5
minato-kei <sweetness.deathcream@gmail.com>
2019-08-27 16:26:45 +0900

    s3画像投稿

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,7 +6,6 @@ class PicturesController < ApplicationController
   end
   
   def create
-    # binding.pry
     @picture = Picture.new(picture_params)
     if @picture.save
       redirect_to root_path
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,4 @@
-# if Rails.env.production?
+if Rails.env.production?
   require 'carrierwave/storage/abstract'
   require 'carrierwave/storage/file'
   require 'carrierwave/storage/fog'
@@ -8,12 +8,12 @@
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
       region: 'ap-northeast-1' 
     }
 
     config.fog_directory  = 'mercari-fake-57c'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercari-fake-57c'
   end
-# end
+end
