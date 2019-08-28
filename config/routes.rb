ac3e1d743c0cdcf1a1516f96a4119decea52b73a
kalugo <bbkb4a3@gmail.com>
2019-08-29 00:15:33 +0900

    link page

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
+  get "items/hidden" => "items#hidden"
   resources :pictures, only: [:new, :create]
+  resources :items, only: [:index, :show]
 end
