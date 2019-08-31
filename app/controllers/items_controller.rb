dbceaed8113611c8d47645d1c52dc1ac0bf37faf
fumitrial8 <fumitrial8@gmail.com>
2019-08-31 18:23:20 +0900

    トップページのプルダウンメニュー機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,11 @@
 class ItemsController < ApplicationController
 
   def index
+    @big_categories = Category.where(ancestry: nil)
     @ladies_items = Category.where(name: "レディース")[0].items.last(4)
     @mens_items = Category.where(name: "メンズ")[0].items.last(4)
     @babies_items = Category.where(name: "ベビー・キッズ")[0].items.last(4)
-    @makeup_items = Category.where(name: "コスメ・香水・美容")[0].items.last(4)
+    @makeup_items = Category.where(name: "コスメ・香水・美容")[0].items.last(4) 
     #pickupブランドを4ブランド x4 itemsで作成
   end
   
