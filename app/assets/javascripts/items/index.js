8090228cb4c6f341074a159e4169755ad97a97dd
fumitrial8 <fumitrial8@gmail.com>
2019-09-02 17:11:59 +0900

    途中

--- app/assets/javascripts/items/index.js
+++ app/assets/javascripts/items/index.js
@@ -1,15 +1,21 @@
 $(function(){
   var items_height = [];
+  $(document).ready(function(){
+    $(".list_item").each(function(){
+      var item = $(this).offset().top;
+      if (scroll_bottom > item) {
+        $(this).animate({opacity: 1}, 1000);
+      }
+    });
+  });
   $(window).on("scroll", function(){
-    
+    var window_size = $(this).innerHeight();
     var scroll_bottom = $(this).scrollTop() + window_size;
     
     $(".list_item").each(function(){
       var item = $(this).offset().top;
-      var window_size = $(this).innerHeight();
-      var scroll_bottom = $(this).scrollTop() + window_size;
       if (scroll_bottom > item) {
-        $(this).
+        $(this).animate({opacity: 1}, 1000);
       }
     });
     
