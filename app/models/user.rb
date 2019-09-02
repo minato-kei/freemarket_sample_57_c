fc410b72ca9229caa434e2d14209df39de82cc74
kalugo <bbkb4a3@gmail.com>
2019-09-02 17:09:20 +0900

    validate user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@ class User < ApplicationRecord
   # has_many :likes, dependent: :destroy
   # has_many :searches, dependent: :destroy
   # has_many :follows, dependent: :destroy
+
+  # validates_with UserValidator
 end
