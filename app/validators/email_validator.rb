96a24c250bd0a3102b3964b699b45d91fe08f65a
kalugo <bbkb4a3@gmail.com>
2019-09-02 20:11:45 +0900

    add validate

---
+++ app/validators/email_validator.rb
@@ -0,0 +1,6 @@
+class UserValidator < ActiveModel::Validator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
--- app/validators/user_validator.rb
+++ app/validators/user_validator.rb
@@ -1,13 +1,6 @@
 class UserValidator < ActiveModel::Validator
-  def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "is not an email")
-    end
-  end
-
   def validate(record)
-    # phone_numberは別ページのため抜いてます。
-    items = [record.nickname, record.email, record.password, record.last_name_kanji, record.first_name_kanji, record.last_name_kana, record.first_name_kana, record.birthday]
+    items = [record.nickname, record.email, record.password, record.last_name_kanji, record.first_name_kanji, record.last_name_kana, record.first_name_kana, record.phone_number, record.birthday]
     return if items.any?(:present?)
     record.errors[:base] << '入力漏れがあります'
   end
