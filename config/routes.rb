f85e256a274004b7211498de708a3f8d0a555616
naru-taka <aloha.46.naru@me.com>
2019-09-02 13:34:48 +0900

    resouces 1行削除

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
-
-  resources :items, only: [:show, :index]
-
+  
   get "items/hidden" => "items#hidden"
 
   resources :pictures, only: [:new, :create]
