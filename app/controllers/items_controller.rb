e8c24df5f0118d722b84aa0f06f5a27e9eea300d
fumitrial8 <fumitrial8@gmail.com>
2019-09-02 14:22:54 +0900

    トップページのitem一覧の表示を記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.all.last(10)
+    @picture = Picture.first
+    #上はアソシエーション組んでいないので、仮おき
     @ladies_items = Category.where(name: "レディース")[0].items.last(4)
     @mens_items = Category.where(name: "メンズ")[0].items.last(4)
     @babies_items = Category.where(name: "ベビー・キッズ")[0].items.last(4)
