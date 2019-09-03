b5d71619a1d3dc120392c91a5da61261ee89c9fb
fumitrial8 <fumitrial8@gmail.com>
2019-09-03 15:39:47 +0900

    アプリケーション全体でbig_category呼び出し

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: proc{Rails.env.production?}
-  
+  before_action :render_header
+
+  def render_header
+    @big_categories = Category.where(ancestry: nil)
+  end
+
   private
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
