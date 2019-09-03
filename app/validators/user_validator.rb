a0159de354eeda3fe9b23d7788d0334b7ddee6c6
kalugo <bbkb4a3@gmail.com>
2019-09-03 13:27:06 +0900

    fix UserValidator

--- app/validators/user_validator.rb
+++ app/validators/user_validator.rb
@@ -1,6 +1,6 @@
 class UserValidator < ActiveModel::Validator
   def validate(record)
-    items = [record.nickname, record.email, record.password, record.last_name_kanji, record.first_name_kanji, record.last_name_kana, record.first_name_kana, record.phone_number, record.birthday]
+    items = [record.nickname, record.email, record.last_name_kanji, record.first_name_kanji, record.last_name_kana, record.first_name_kana, record.phone_number, record.birthday]
     return if items.any?(:present?)
     record.errors[:base] << '入力漏れがあります'
   end
