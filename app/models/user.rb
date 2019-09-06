859795b2903f4fc06ef0802c32b72a486b69628a
minato-kei <sweetness.deathcream@gmail.com>
2019-09-06 15:25:14 +0900

    コメントアウト消し忘れを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
-  # omniauthのコールバック時に呼ばれるメソッド
   
   has_many :shippings, dependent: :destroy
   has_many :items, dependent: :destroy
@@ -17,22 +16,6 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.nickname = auth.info.name 
-      
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
     end
   end
-  # def self.find_for_oauth(auth)
-  #   user = User.where(uid: auth.uid, provider: auth.provider).first
-
-  #   unless user
-  #     user = User.create(
-  #       uid: auth.uid,
-  #       provider: auth.provider,
-  #       email: auth.info.email,
-  #       password: Devise.friendly_token[0, 20]
-  #     )
-  #   end
-  #   user
-  # end
 end
