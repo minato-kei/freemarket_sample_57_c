36005623d1d9e27ba345e35239ac00612974888a
kalugo <bbkb4a3@gmail.com>
2019-09-07 19:31:50 +0900

    sidebar reset btn

--- app/assets/javascripts/items/_search.js
+++ app/assets/javascripts/items/_search.js
@@ -2,7 +2,6 @@ $(document).on('turbolinks:load', function() {
   $('#search-category-root').on('change', function(e){
     var id = $('#search-category-root option:selected').val();
     if (id.length==0){
-      console.log("is blank")
       $(".search-category-child").siblings().addClass("hide")
     }
     $(`#category_${id}`).removeClass("hide")
@@ -12,12 +11,13 @@ $(document).on('turbolinks:load', function() {
   });
   $('.search-category-child').on('change',function(e){
     var id = $(this).val();
-    console.log(`${id}`);
     if (id.length==0){
       $(this).siblings("div").addClass("hide")
     }
     $(`#category_${id}`).removeClass("hide")
     $(`#category_${id}`).siblings("div").addClass("hide")
   });
-
+  $('#search__reset').on('click', function(e){
+    $('#category__child').children().addClass("hide")
+  });
 });
