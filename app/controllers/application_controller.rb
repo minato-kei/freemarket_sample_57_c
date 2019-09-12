cc27f5eb70c728ba59be2020f530ac4416e08bff
minato-kei <sweetness.deathcream@gmail.com>
2019-09-12 21:41:25 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   before_action :basic_auth, if: proc{Rails.env.production?}
   before_action :render_header
   before_action :current_user
