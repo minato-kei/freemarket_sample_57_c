1629e23a6a1b545948fcea4bbc5c480cc1d65778
minato-kei <sweetness.deathcream@gmail.com>
2019-09-17 21:25:44 +0900

    items-controller単体テスト記述

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,18 +1,52 @@
 require 'rails_helper'
 describe ItemsController do
-  
-  describe 'GET #index' do
-  
-    it "@itemsという変数が正しく定義されているか" do
-      @items = create_list(:item, 4, user_id: user.id, category_id: categories.first.id, size_id: sizes.first.id, status_id: statuses.first.id)
-      get :index
-      expect(assigns(:items)).to eq(@items)
-    end
 
-    it "# 該当するビューが描画されているか" do
+  describe 'GET #index' do
+    it "renders the :index template" do
       get :index
       expect(response).to render_template :index
     end
   end
 
+  describe "GET #new" do
+    it "renders the :new template" do
+      user = create(:user)
+      sign_in user
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe "GET #show" do
+    it "responds successfully" do
+      item = create(:item)
+      user = create(:user)
+      get :show, params: {id: item}
+      expect(response).to render_template :show  
+    end
+    
+    it "returns a 200 response" do
+      item = create(:item)
+      user = create(:user)
+      get :show, params: {id: item}
+      expect(response).to have_http_status "200"
+    end
+  end
+
+  
+  describe 'GET #edit' do
+    it "assigns the requested item to @item" do
+      item = create(:item)
+      user = create(:user)
+      get :edit, params: { id: item}
+      expect(assigns(:item)).to eq item
+    end
+    it "renders the :edit template" do
+      item = create(:item)
+      user = create(:user)
+      sign_in user
+      get :edit,params: {id:item}
+      expect(response).to render_template :edit      
+    end
+  end
 end
