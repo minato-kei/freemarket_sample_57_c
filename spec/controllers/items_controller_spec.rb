35d1b27db5192e52ca64b59a00b947138ee88c7a
minato-kei <sweetness.deathcream@gmail.com>
2019-09-17 11:16:51 +0900

    テスト時deviseログイン

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,3 +1,18 @@
 require 'rails_helper'
 describe ItemsController do
+  
+  describe 'GET #index' do
+  
+    it "@itemsという変数が正しく定義されているか" do
+      @items = create_list(:item, 4, user_id: user.id, category_id: categories.first.id, size_id: sizes.first.id, status_id: statuses.first.id)
+      get :index
+      expect(assigns(:items)).to eq(@items)
+    end
+
+    it "# 該当するビューが描画されているか" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
 end
